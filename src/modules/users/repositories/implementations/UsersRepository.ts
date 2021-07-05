c2c59fb38f0a4413e63d161602f18fc8b8f876d4
jose guilherme <jgnvs6@gmail.com>
2021-07-05 10:21:57 -0300

    feat: implement user repository

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,22 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = this.repository.findOneOrFail(user_id, {
+      relations: ['games']
+    })
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name, last_name ASC; ');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository
+    .query(`SELECT * FROM users WHERE CONCAT(LOWER(first_name),' ',LOWER(last_name)) = '${(first_name.toLowerCase() + ' ' + last_name.toLowerCase())}'; `);
   }
 }
